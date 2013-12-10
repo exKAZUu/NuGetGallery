29571bb5508feec2298314dc1ba48af5873bc39d
bhuvak <bhuvak@live.com>
2013-12-10 15:13:31 -0800

    TempFixes

--- src/NuGetGallery/Scripts/stats.js
+++ src/NuGetGallery/Scripts/stats.js
@@ -1,4 +1,6 @@
-﻿function getStats(currData) {
+﻿debugger
+function getStats(currData) {
+   
     currData = currData || {};
     $.get('/stats/totals', function(data) {
         var section = $('section.aggstats');
@@ -11,7 +13,7 @@
         section.show();
     });
 
-    setTimeout(function () { getStats(currData); }, 30000);
+    setTimeout(function () { getStats(currData); }, 10000);
 }
 
 function update(data, currData, key) {
